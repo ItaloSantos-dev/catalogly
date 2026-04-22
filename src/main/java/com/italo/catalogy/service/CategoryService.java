0c682f92ccfc7b330aa506fd2a1e3c3c02bc10c6
Italo Santos <italosantoscont@gmail.com>
2026-04-22 17:39:30 -0300

    feat:(category): in create category add function for add items of CreateCategoryrequestDTO

--- src/main/java/com/italo/catalogy/service/CategoryService.java
+++ src/main/java/com/italo/catalogy/service/CategoryService.java
@@ -4,10 +4,14 @@ import com.italo.catalogy.dto.category.CreateCategoryRequestDTO;
 import com.italo.catalogy.mapper.CategoryMapper;
 import com.italo.catalogy.model.CatalogModel;
 import com.italo.catalogy.model.CategoryModel;
+import com.italo.catalogy.model.ItemModel;
 import com.italo.catalogy.respository.CatalogRepository;
 import com.italo.catalogy.respository.CategoryRepository;
+import com.italo.catalogy.respository.ItemRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,13 +19,16 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final CatalogRepository catalogRepository;
     private final CategoryMapper categoryMapper;
+    private final ItemRepository itemRepository;
 
-    public CategoryService(CategoryRepository categoryRepository, CatalogRepository catalogRepository, CategoryMapper categoryMapper) {
+    public CategoryService(CategoryRepository categoryRepository, CatalogRepository catalogRepository, CategoryMapper categoryMapper, ItemRepository itemRepository) {
         this.categoryRepository = categoryRepository;
         this.catalogRepository = catalogRepository;
         this.categoryMapper = categoryMapper;
+        this.itemRepository = itemRepository;
     }
 
+    @Transactional
     public CategoryModel createCategory(CreateCategoryRequestDTO createCategoryRequestDTO, UUID userId){
         CatalogModel catalogModel = this.catalogRepository.findBySellerUserId(userId)
                 .orElseThrow(() -> new RuntimeException("Deu ruin"));
@@ -29,7 +36,8 @@ public class CategoryService {
         CategoryModel newCategory = this.categoryMapper.createToModel(createCategoryRequestDTO);
 
         if (createCategoryRequestDTO.items()!=null && !createCategoryRequestDTO.items().isEmpty()){
-            //Relacionar items com a categoria
+            List<ItemModel> items =  this.itemRepository.findAllById(createCategoryRequestDTO.items());
+            newCategory.setItems(items);
         }
         newCategory.setCatalog(catalogModel);
         return this.categoryRepository.save(newCategory);
