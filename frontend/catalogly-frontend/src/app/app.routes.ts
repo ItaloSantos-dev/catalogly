223a0937cd9cd0665ec08793da58492ea23f2606
Italo Santos <italosantoscont@gmail.com>
2026-05-31 10:16:15 -0300

    feat(layout): create a lyout for all pages of project

--- frontend/catalogly-frontend/src/app/app.routes.ts
+++ frontend/catalogly-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { PageLayout } from './components/page/page-layout/page-layout';
+import { PageHome } from './components/page/page-home/page-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:PageLayout,
+        children: [
+            {
+                path:'',
+                component: PageHome
+            }
+        ]
+    }
+];
